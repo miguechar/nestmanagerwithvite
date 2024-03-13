711ce76b4893d28e31ef96bf294ebb4d1afcc729
Miguel Charry <miguelecharry11@gmail.com>
2024-03-13 06:47:37 -0500

    delete dir

--- src/Components/nextui/ItemCounter.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ChevronRightIcon } from "../icons/ChevronRightIcon";
-
-export const ItemCounter = ({number, clickEvent}) => (
-  <div className="flex items-center gap-1 text-default-400">
-    <span className="text-small">{number}</span>
-    <ChevronRightIcon className="text-xl" onClick={() => clickEvent()} />
-  </div>
-);
--- src/Pages/FabricationRequests/Index.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const FabricationRequests = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
